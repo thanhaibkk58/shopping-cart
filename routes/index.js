1dbb43955de4d1cf0f82e70e864d92a6ccb2d3b6
Háº£i ThÃ¢n VÄ?n <thanhaibkk58@gmail.com>
2017-09-26 01:10:41 +0700

    02-MongoDB

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require("express");
 var router = express.Router();
+var Product = require("../models/product");
 
 /* GET home page. */
 router.get("/", function(req, res, next) {
-  res.render("index", { title: "shopping-cart" });
+  Product.find(function (err, docs) {
+      var products = [];
+      products = docs;
+      res.render("index", { title: "shopping-cart", products: products });
+  });
 });
 
 module.exports = router;
